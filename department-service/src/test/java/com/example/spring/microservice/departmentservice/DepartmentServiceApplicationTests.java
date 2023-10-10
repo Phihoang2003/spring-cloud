4ba775f23caa19cf1f26aad30a494dc94b7d2b33
Dang Hoang Phi <hoangphi1232003@gmail.com>
2023-10-10 17:10:41 +0700

    create eureka server and department service

---
+++ department-service/src/test/java/com/example/spring/microservice/departmentservice/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.microservice.departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ server-registry/server-registry/src/test/java/com/example/spring/microservice/serverregistry/ServerRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.microservice.serverregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
